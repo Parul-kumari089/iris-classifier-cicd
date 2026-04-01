07819a772d711941d5ee68f37ca5392ebdf28258
Parul-kumari089 <parulluharuka2005@gmail.com>
2026-04-01 11:11:38 +0530

    Initial ML CI/CD project

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+ 
+# Load the trained model
+model = joblib.load('iris_model.pkl')
+ 
+#initialize the Flask application
+app = Flask(__name__)
+ 
+@app.route("/")
+def home():
+    return "Welcome to the Iris Prediction API! Use the /predict endpoint to get predictions."
+ 
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # get json data from the request
+        data = request.get_json(force=True)
+
+        if "features" not in data or len(data["features"]) != 4:
+            return jsonify({"error": "Invalid input. Exactly 4 features required."}), 400
+ 
+        # extract features from the json data
+        features = np.array(data['features']).reshape(1, -1)
+ 
+        # make a prediction using the loaded model
+        prediction = model.predict(features)[0]
+ 
+        # map the prediction with the corresponding iris class
+        classes = ['setosa', 'versicolor', 'virginica']
+        result = {"prediction": classes[prediction]}
+ 
+        # return the prediction as a json response
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
